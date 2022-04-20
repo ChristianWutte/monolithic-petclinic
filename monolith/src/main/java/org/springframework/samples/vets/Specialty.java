99ffc4e6746f1a0a4d3f72e507bc2ab793e39cc0
Christian Wutte <christian.wutte@dedalus-group.com>
2022-04-20 11:15:32 +0200

    clean-up - remove obsolete imports

--- monolith/src/main/java/org/springframework/samples/vets/Specialty.java
+++ monolith/src/main/java/org/springframework/samples/vets/Specialty.java
@@ -19,7 +19,6 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import org.springframework.samples.petclinic.model.NamedEntity;
-import org.springframework.samples.vets.Vet;
 
 /**
  * Models a {@link Vet Vet's} specialty (for example, dentistry).
